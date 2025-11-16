510340a4d79e18228529060a73624dce5400606d
Nayara Louback <66848069+NayaraLF@users.noreply.github.com>
2025-11-16 14:16:51 +0000

    novos testes

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,4 +1,17 @@
-import { exportTraceState } from "next/dist/trace";
-test("espero que 1 seja 1", () => {
-  expect(1).toBe(1);
+const { TestEnvironment } = require("jest-environment-jsdom");
+const calculadora = require("../models/calculadora.js");
+
+test("somar 2+2 deveria retornar 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
+});
+
+test("somar 5+100 deveria retornar 105", () => {
+  const resultado = calculadora.somar(5, 100);
+  expect(resultado).toBe(105);
+});
+
+test("somar 'banana' deveria retornar 'Erro'", () => {
+  const resultado = calculadora.somar("banana", 100);
+  expect(resultado).toBe("Erro");
 });
