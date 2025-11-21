1bbf11d91683741c525f852766db573da5f72f9c
Nayara Louback <66848069+NayaraLF@users.noreply.github.com>
2025-11-21 17:39:45 +0000

    testes

--- tests/unit/calculadora.test.js
+++ tests/unit/calculadora.test.js
@@ -1,5 +1,5 @@
 const { TestEnvironment } = require("jest-environment-jsdom");
-const calculadora = require("../models/calculadora.js");
+const calculadora = require("../../models/calculadora.js");
 
 test("somar 2+2 deveria retornar 4", () => {
   const resultado = calculadora.somar(2, 2);
